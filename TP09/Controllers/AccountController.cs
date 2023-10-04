5c2acec017ee9c2bd466efde6058c25903e9c209
TOMAS NEIMERMAN GROSSO <47496739@ort>
2023-10-04 09:01:38 -0300

    2

---
+++ TP09/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TP09.Models;
+
+namespace TP09.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _logger;
+
+    public AccountController(ILogger<AccountController> logger)
+    {
+        _logger = logger;
+    }
+    public IActionResult Login(){
+        return View();
+    }
+    public IActionResult Registro(){
+        return View();
+    }
+    public IActionResult GuardarCuenta(Usuario user)
+    {
+       
+        BD.AgregarUsuario(user);    
+        return View("Bienvenida");
+        
+    }
+    public IActionResult Bienvenida(){
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
+
--- TP09/Models/BD.cs
+++ TP09/Models/BD.cs
@@ -5,15 +5,22 @@ using System.Collections.Generic;
 namespace TP09.Models;
 public class BD{
 
-    private static string _connectionString = @"Server=.; Database=Usuario; Trusted_Connection=True";
+    private static string _connectionString = @"Server=A-PHZ2-CIDI-049; Database=Usuario; Trusted_Connection=True";
     
     public static void AgregarUsuario (Usuario u){
         string sql = "INSERT INTO Usuario (NombreUsuario, Contraseña, Email, Telefono, Nombre, Apellido)" + "VALUES (@Usuario, @Contraseña, @Email, @Telefono, @Nombre, @Apellido);";
         using(SqlConnection conexion = new SqlConnection(_connectionString)){
-            conexion.Execute(sql,u);
+            conexion.Execute(sql,new{
+                Usuario = u.NombreUsuario,
+                Contraseña = u.Contraseña,
+                Email = u.Email,
+                Telefono = u.Telefono,
+                Nombre = u.Nombre,
+                Apellido = u.Apellido
+            });
         }
     }
-    public static void cambiarContrasena (Usuario u, string user)
+    public static void cambiarContraseña (Usuario u,string user)
     {
         string sql = "UPDATE Usuario SET Contraseña = @contrasena, Contraseña = @nuevaContrasena2 WHERE @Usuario = user;"; 
         using(SqlConnection conexion = new SqlConnection(_connectionString))
