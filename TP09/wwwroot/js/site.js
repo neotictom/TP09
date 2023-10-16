6089f7d661f5850e075420f0251dd3cbdaa25da3
neotictom <128648747+neotictom@users.noreply.github.com>
2023-10-15 21:53:32 -0300

    Update site.js

--- TP09/wwwroot/js/site.js
+++ TP09/wwwroot/js/site.js
@@ -1,1 +1,9 @@
-﻿
+function validarContraseña() {
+    var contraseña = document.getElementById("password").value;
+    var regex = /^(?=.*[A-Z])(?=.*[!@#$%^&*])(.{8,})$/;
+    if (!regex.test(contraseña)) {
+        alert("La contraseña debe tener al menos 8 caracteres, una letra en mayúscula y un carácter especial (!@#$%^&*).");
+        return false;
+    }
+    return true;
+}
